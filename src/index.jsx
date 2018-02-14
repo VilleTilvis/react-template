dbf38643030bd2b56c191847aa82cda636b939c5
Ville Tilvis <Ville.Tilvis@mayk.fi>
2018-02-14 16:30:52 +0200

    Otsikko isoksi

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Ville sanoo moi!</div>
+  <div><h1>Ville sanoo moi!</h1></div>
 );
 
 class App extends Component {
